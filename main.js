3417fc5f3fdddcf59a9cdd65a98cdf5f98d2beb6
tubi55 <tubi55@nate.com>
2021-09-25 14:06:38 +0900

    setTimeout을 활용한 모션중 재이벤트 방지

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var $wrap = $("#wrap");
 var $btns = $wrap.find(".left li");
 var $boxs = $wrap.find(".right div");
+var enableClick = true;
 
 $btns.on("click",function(e){
     e.preventDefault();
@@ -8,14 +9,31 @@ $btns.on("click",function(e){
     var isOn = $(this).hasClass("on");
     if(isOn) return;
 
-    var i = $(this).index();
-    activation(i);    
+    if(enableClick){
+        enableClick = false;
+        var i = $(this).index();
+        activation(i);
+    }        
 });
 
 function activation(index){ 
+    console.log("start!!");
     $btns.removeClass("on");
     $btns.eq(index).addClass("on");
 
     $boxs.removeClass("on");
     $boxs.eq(index).addClass("on");
+
+    setTimeout(function(){
+        enableClick = true;
+    },4000);
 }
+
+/*   
+    특정 시간 만큼 코드 지연시키기
+    setTimeout(실행할 함수, 지연시간);
+
+    setTimeout(function(){
+        이곳의 구문이 2초 있다 실행됨
+    },2000);
+*/
